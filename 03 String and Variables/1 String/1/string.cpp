a33026029d487d32efbeb4b49fce627939cc8ec1
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2024-01-08 17:53:16 +0700

    Add files via upload

---
+++ 03 String and Variables/1 String/1/string.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+	std::cout << "Good Morning\n";
+	std::cout << "Good Afternoon";
+	std::cout << "Good Evening";
+	std::cout << " Good Night" << std::endl;
+	std::cin.get();
+	return 0;
+}
