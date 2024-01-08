e9343d2c4cb41591c0b7815abce03b3e136c5408
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2024-01-08 17:53:30 +0700

    Add files via upload

---
+++ 04 Input and Output/2 Input Program/input.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std; 
+
+int main(){
+
+	// Create Input
+	int a;
+	int b;
+	cout << "Insert Value 1: ";
+	cin >> a;
+	cout << "Insert Value 2: ";
+	cin >> b;
+
+	cout << "Value 1 : ";
+	cout << a << endl;
+	cout << "Value 2 : ";
+	cout << b << endl;
+	cout << "Value 1 + Value 2 : ";
+	cout << a + b << endl;
+
+	cin.get();
+	return 0;
+}
