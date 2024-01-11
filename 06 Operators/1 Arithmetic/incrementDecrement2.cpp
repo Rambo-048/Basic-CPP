18ade35ebb35ee06145e99cfe938d77671c7079e
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2024-01-11 09:48:15 +0700

    Add files via upload

---
+++ 06 Operators/1 Arithmetic/incrementDecrement2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 5;
+	int b = 5;
+
+	// Postincrement
+	cout << a << endl;             // a -> 5
+	cout << a++ << endl;           // postincrement --> (a = 5) --> (a + 1)
+	cout << a << endl << endl;     // a ->
+
+	// preincrement
+	cout << b << endl;             // a -> 5
+	cout << ++b << endl;           // preincrement  --> (a + 1 = 6)
+	cout << b << endl << endl;     // a -> 6
+	
+	// Postdecrement
+	cout << a << endl;             // a -> 6
+	cout << a-- << endl;           // postdecrement --> (a = 6) --> (a - 1)
+	cout << a << endl << endl;     // a -> 5
+
+	// predecrement
+	cout << b << endl;             // a -> 6
+	cout << --b << endl;           // predecrement  --> (a - 1 = 5)
+	cout << b << endl << endl;     // a -> 5
+
+	cin.get();
+	return 0;
+}
