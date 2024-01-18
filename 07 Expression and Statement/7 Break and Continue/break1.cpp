6ddaba633c7a5a37ff17a68b536cb944837a6500
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2024-01-18 12:17:04 +0700

    Add files via upload

---
+++ 07 Expression and Statement/7 Break and Continue/break1.cpp
@@ -0,0 +1,18 @@
+// Not Use Break and Continue
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{	
+	for(int i = 1; i <= 10; i++){
+		cout << i << endl;
+	} 
+
+	cout << "Finish" << endl;
+
+	cin.get();
+	return 0;
+}
+
---
+++ 07 Expression and Statement/7 Break and Continue/break2.cpp
@@ -0,0 +1,19 @@
+// Use Break
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	for (int i = 1; i <= 10; i++){
+		cout << i << endl;
+		if (i == 5) {
+			break;
+		}
+	}
+	cout << "Finish" << endl;
+
+	cin.get();
+	return 0;
+}
