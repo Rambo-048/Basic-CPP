cbbeab5d4a9c2ed629b1bab9f8ebaf8896346e1e
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-12 13:22:07 +0700

    Add files via upload

---
+++ 05 Data Type/4 Boolean/boolean2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bits/stdc++.h> // Standar Library - Numeric Limits
+
+using namespace std;
+
+int main() {
+	// Fundamental Data Type :
+
+	// Boolean
+	bool a = true; // true=1, false=0
+	bool b = false; // true=1, false=0
+	bool c = true; // true=1, false=0
+	bool d = false; // true=1, false=0
+
+	cout << a << endl;
+	cout << sizeof(a) << " byte" << endl;
+	cout << numeric_limits<int>::max() << endl;
+	cout << numeric_limits<int>::min() << endl;
+	cin.get();
+	return 0;
+}
