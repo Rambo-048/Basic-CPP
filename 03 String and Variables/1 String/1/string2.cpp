5db9341814dc94bf810a54ce401e5106526ce3f8
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-12 12:16:57 +0700

    Add files via upload

---
+++ 03 String and Variables/1 String/1/string2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+	std::cout << "We Are Not Perfect, But We Make A Perfect Combination\n";
+	std::cout << "Dreams And Dedication Are A Powerful Combination";
+	std::cout << "Honest Feeling And Bad Timing Make The Most Painful Combination";
+	std::cout << " Perfect Combination Are Rare In An Imperfect World" << std::endl;
+	std::cin.get();
+	return 0;
+}
