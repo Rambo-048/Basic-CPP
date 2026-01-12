c77c80592ea564363a227e7ca3b3fb0443f16a98
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-12 11:28:40 +0700

    Add files via upload

---
+++ 02 Compile and Linking/1 Compile/compile3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int a = 10;
+int b = 10;
+
+int main () 
+{
+	std::cout << a + b << std::endl;
+	return 0;
+}
---
+++ 02 Compile and Linking/1 Compile/compile5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int a = 5;
+int b = 5;
+int c = 5;
+
+int main () 
+{
+	std::cout << a * b * c << std::endl;
+	return 0;
+}
