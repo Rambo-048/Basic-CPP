0041947b938ce29f6527ce06d779b73386ac1bc0
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-12 12:17:57 +0700

    Add files via upload

---
+++ 03 String and Variables/2 Variables/2/variables2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+
+	int a = 5;
+	int b = 10;
+	int c = 15;
+	int c = 20;
+
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << c << std::endl;
+	std::cout << d << std::endl;
+	std::cin.get();
+	return 0;
+}
