44ed1e4a1924a0cbc3d63eca93c0504f149e5bb1
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-12 12:17:16 +0700

    Add files via upload

---
+++ 03 String and Variables/1 String/2/string2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+	std::cout << "Good Timing Is Invisible, Bad Timing Sticks Out A Mile\n";
+	std::cout << "Maybe Not Good Timing Means, There's No Bad Timing Either\n";
+	std::cout << "Our Love Was Right, But The Timing Was Painfully Wrong\n";
+	std::cout << " If The Hearts Matters Enough Timing Doesn't" << std::endl;
+	std::cin.get();
+	return 0;
+}
