9baeb7bd7391dc380a8cfb0a30cf7829cfb78e86
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-12 13:21:49 +0700

    Add files via upload

---
+++ 05 Data Type/3 Character/char2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h> // Standar Library - Numeric Limits
+
+using namespace std;
+
+int main() {
+	// Fundamental Data Type :
+
+	// Character
+	char a = "E"; // character 1 byte
+	char b = "D"; // character 1 byte
+	char c = "C"; // character 1 byte
+	char d = "B"; // character 1 byte
+        char e = "A"; // character 1 byte
+
+
+	cout << a << endl;
+	cout << sizeof(a) << " byte" << endl;
+	cout << numeric_limits<int>::max() << endl;
+	cout << numeric_limits<int>::min() << endl;
+	cin.get();
+	return 0;
+}
