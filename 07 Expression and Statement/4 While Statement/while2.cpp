3a79c1151bf61321e7b03f032346504fd4efb7b6
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-14 12:46:48 +0700

    Add files via upload

---
+++ 07 Expression and Statement/4 While Statement/while2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int a = 10;
+
+	while (a <= 20) {
+		cout << "BECAUSE I CARRY IT WELL DOESN'T MEAN IT'S NOT HEAVY";
+		cout << a << endl << endl;
+		a += 1;
+	}
+
+	cout << "Finish" << endl;
+
+	cin.get();
+	return 0;
+}
---
+++ 07 Expression and Statement/4 While Statement/while3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int a = 15;
+I
+	while (a <= 20) {
+		cout << "I DON'T TRUST EASILY, SO WHEN I TELL YOU, I TRUST YOU, PLEASE DON'T MAKE ME REGRET IT";
+		cout << a << endl << endl;
+		a += 1;
+	}
+
+	cout << "Finish" << endl;
+
+	cin.get();
+	return 0;
+}
