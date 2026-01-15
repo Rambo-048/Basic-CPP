b62acf3b04f44561f243d2eab538b4059d359dab
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-15 13:04:15 +0700

    Update strings3.cpp

--- 11 Strings/strings3.cpp
+++ 11 Strings/strings3.cpp
@@ -13,16 +13,16 @@ int main()
 
 	// Change char/alphabet from Indeks
 	cout << "Index 0 : " << alphabet[0] << endl;
-	// cout << "Index 1 : " << alphabet[0][1] << endl;
-	// cout << "Index 2 : " << alphabet[0][1][2] << endl;
-	// cout << "Index 3 : " << alphabet[0][1][2][3] << endl;
-	// cout << "Index 4 : " << alphabet[0][1][2][3][4] << endl;
-	// cout << "Index 5 : " << alphabet[0][1][2][3][4][5]<< endl;
-	// cout << "Index 6 : " << alphabet[0][1][2][3][4][5][6] << endl;
-	// cout << "Index 7 : " << alphabet[0][1][2][3][4][5][6][7] << endl;
-	// cout << "Index 8 : " << alphabet[0][1][2][3][4][5][6][7][8] << endl;
-	// cout << "Index 9 : " << alphabet[0][1][2][3][4][5][6][7] << endl;
-	// cout << "Index 10: " << alphabet[0][1][2][3][4][5][6] << endl;
+	// cout << "Index 1  : " << alphabet[0][1] << endl;
+	// cout << "Index 2  : " << alphabet[0][1][2] << endl;
+	// cout << "Index 3  : " << alphabet[0][1][2][3] << endl;
+	// cout << "Index 4  : " << alphabet[0][1][2][3][4] << endl;
+	// cout << "Index 5  : " << alphabet[0][1][2][3][4][5]<< endl;
+	// cout << "Index 6  : " << alphabet[0][1][2][3][4][5][6] << endl;
+	// cout << "Index 7  : " << alphabet[0][1][2][3][4][5][6][7] << endl;
+	// cout << "Index 8  : " << alphabet[0][1][2][3][4][5][6][7][8] << endl;
+	// cout << "Index 9  : " << alphabet[0][1][2][3][4][5][6][7] << endl;
+	// cout << "Index 10 : " << alphabet[0][1][2][3][4][5][6] << endl;
 	// cout << "Index 11 : " << alphabet[0][1][2][3][4][5] << endl;
 	// cout << "Index 12 : " << alphabet[0][1][2][3][4] << endl;
 	// cout << "Index 13 : " << alphabet[0][1][2][3] << endl;
