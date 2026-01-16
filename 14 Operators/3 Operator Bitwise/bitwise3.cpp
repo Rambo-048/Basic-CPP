efcaf322973f7277c66d3fcd105b3c02e8ef1f55
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-16 13:41:49 +0700

    Add files via upload

---
+++ 14 Operators/3 Operator Bitwise/bitwise3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bitset>
+#include <string>
+
+using namespace std;
+
+void printBinary(unsigned short val, string name){
+	cout << name << " = " << bitset<8>(val) << endl;
+}
+
+int main(){
+
+	unsigned short a = 30;
+
+	cout << bitset <8>(a) << endl;
+
+	cin.get();
+	return 0;
+}
