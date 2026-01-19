db29eef46c632f9ce63cf157b8433fc659b4d9a9
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-19 13:23:43 +0700

    Add files via upload

---
+++ 17 Preprocessing Directive/5 ifdefine ifundefine pragma/ifdef2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+// #include <string>
+  
+#include "dataStruct.h"
+// #include "foo.h"
+// #include "bar.h"
+
+// // Example 1 ifdef 
+// #define FOO
+// #ifdef FOO
+// 	#define TEST_FOO "foo found"
+// #else
+// 	#define TEST_FOO "foo not found"
+// #endif
+
+
+// // Example 2 ifndef      ----> If Data Not Define
+// #define FOO "Old Foo"
+
+// #ifndef FOO
+// #define FOO "New Foo"  // Foo Baru akan ditampilkan jika Foo Lama di atas kita hilangkan atau hapus.
+// #endif
+
+
+// #define FOO "Old Foo"
+
+
+// pragma once
+
+
+using namespace std;
+
+int main(){
+
+	Movies movie;
+	movie.id_number : 7536801;
+	movie.actor_name : "Sanctuary";
+	movie.type : "Fighter";
+
+	cout << movie.id_number << endl;
+	cout << movie.actionFigure_name << endl;
+	cout << movie.type << endl;
+
+	cin.get();
+	return 0;
+}
