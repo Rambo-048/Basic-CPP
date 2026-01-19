ad460ecf5abb548acc0af4cc23650f2a05b45939
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-19 13:23:22 +0700

    Add files via upload

---
+++ 17 Preprocessing Directive/4 include/include2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "library2.h"
+#include <string>
+
+#define PI 3.14159265359
+
+using namespace std;
+
+int main(){
+	cout << "Congratulations..!!" << endl;
+
+	cout << "PI : " << PI << endl;
+	cout << "DEGREE: " << DEGREE(5) << endl;
+	cout << "MAX: " << MAX(20,5) << endl;
+	cout << "Data String: " << data << endl;
+	cin.get();
+	return 0;
+}
---
+++ 17 Preprocessing Directive/4 include/library2.h
@@ -0,0 +1,7 @@
+#include <string>
+
+#define PI 3.14159265359
+#define DEGREE(X) (X*X)
+#define MAX(A,B) ((A > B) ? A:B)
+
+std::string data = "Nobody Said It Would Be Easy, So Don't Ever Give Up When It Gets Hard";
