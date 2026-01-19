bcc7c74d74e5a1adb21fcbe8b48d932dd9bce7a1
Rambo-048 <85503485+Rambo-048@users.noreply.github.com>
2026-01-19 13:24:09 +0700

    Add files via upload

---
+++ 17 Preprocessing Directive/6 namespace/library.h
@@ -0,0 +1,11 @@
+namespace test {
+	int b = 30;
+
+	void function(){
+		std::cout << "Live Your Life To The Fullest And Enjoy Every Moment Of It" << std::endl;
+	}
+
+	void cout(int a){
+		std::cout << a << std::endl;
+	}
+}
---
+++ 17 Preprocessing Directive/6 namespace/namespace2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void function(){
+		std::cout << "Nobody Said It Would Be Easy, So Don't Ever Give Up When It Gets Hard" << std::endl;
+	}
+
+int main(){
+	function();
+	std::cout << library::b << std::endl;
+	library::function();
+	library::cout(500);
+	std::cin.get();
+	return 0;
+}
